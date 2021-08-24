711e62117e2c2732a846564f4bcd19dad6a840a0
chicoyaki01 <chicoyaki01@gmail.com>
2021-08-24 23:21:47 +0900

    update

--- bspwm.sh
+++ bspwm.sh
@@ -1,3 +1,11 @@
+it clone https://aur.archlinux.org/yay.git
+cd ~
+cd yay
+makepkg -si
+cd ../
+rm -rf yay
+yay -S google-chrome yadm kime
+
 yadm clone https://github.com/Chicoyaki01/bspwm.git
 yadm pull #깃에서 내가 올린거 가져옴
 yadm alt #가져온거 적용
