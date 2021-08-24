e8d88d9e366826d02eb112f4b9ebb0e9c43fda56
chicoyaki01 <chicoyaki01@gmail.com>
2021-08-25 02:06:15 +0900

    Polybar

--- bspwm.sh
+++ bspwm.sh
@@ -5,7 +5,7 @@ makepkg -si
 cd ../
 rm -rf yay
 
-yay -S google-chrome yadm kime
+yay -S google-chrome yadm kime polybar
 
 yadm clone https://github.com/Chicoyaki01/bspwm.git
 yadm pull #깃에서 내가 올린거 가져옴
