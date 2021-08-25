bc9626df0334daf1684e9040c1a3fd8676ed9ffc
chicoyaki01 <chicoyaki01@gmail.com>
2021-08-26 03:17:18 +0900

    update

--- auto.sh
+++ auto.sh
@@ -35,7 +35,7 @@ echo "You have to edit 'sudo nano /etc/sudoers'."
 echo "#%wheel ALL= <----- remove #"
 echo "%wheel ALL= <---- Look like this"
 
-sudo pacman -S xorg bspwm sxhkd rofi alacritty lightdm lightdm-gtk-greeter 
+sudo pacman -S xorg bspwm sxhkd rofi alacritty lightdm lightdm-gtk-greeter pulseaudio pavucontrol
 sudo pacman -S lightdm-gtk-greeter-settings nitrogen lxappearance picom nautilus 
 sudo pacman -S noto-fonts noto-fonts-cjk xorg-fonts-misc
 sudo systemctl enable lightdm
