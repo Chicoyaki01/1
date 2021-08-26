2e0e52cb4b2ad7e7d368fbdb731305eae109842d
chicoyaki01 <chicoyaki01@gmail.com>
2021-08-27 02:39:42 +0900

    e

--- bspwm.sh
+++ bspwm.sh
@@ -7,7 +7,7 @@ rm -rf yay
 
 yay -S google-chrome yadm kime polybar ttf-unifont siji-git nerd-fonts-complete spotify zscroll-git
 
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 yadm clone https://github.com/Chicoyaki01/bspwm.git
 yadm pull #깃에서 내가 올린거 가져옴
